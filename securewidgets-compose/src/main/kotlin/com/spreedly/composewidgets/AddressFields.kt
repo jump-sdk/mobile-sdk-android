718218c4c76fac36f6bd52974ec8e7dd7e513bda
Nicolas Langheit <35898659+nlangheit@users.noreply.github.com>
2025-08-05 15:44:01 -0400

    Add address field collection (#10)
    * add address field to edit and text field
    
    * fix build
    
    * update java version to 21
    
    * bump detekt
    
    * pr comment
    
    ---------
    
    Co-authored-by: Brandon Roeder <brandon@jump.com>

---
+++ securewidgets-compose/src/main/kotlin/com/spreedly/composewidgets/AddressFields.kt
@@ -0,0 +1,124 @@
+package com.spreedly.composewidgets
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material.TextFieldColors
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.autofill.AutofillType
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.text.TextStyle
+import com.spreedly.composewidgets.utils.PaddableOutlinedTextField
+import com.spreedly.composewidgets.utils.autofill
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun StreetAddressField(
+    onValueChange: (String) -> Unit,
+    textStyle: TextStyle,
+    shape: Shape,
+    colors: TextFieldColors,
+    textFieldPadding: PaddingValues,
+    modifier: Modifier = Modifier,
+    initialValue: String = "",
+    label: @Composable () -> Unit,
+) {
+    var value by rememberSaveable { mutableStateOf(initialValue) }
+
+    PaddableOutlinedTextField(
+        modifier = modifier.autofill(
+            autofillTypes = listOf(AutofillType.AddressStreet),
+            onFill = {
+                value = it
+                onValueChange(it)
+            },
+        ),
+        value = value,
+        onValueChange = {
+            value = it
+            onValueChange(it)
+        },
+        label = label,
+        textStyle = textStyle,
+        shape = shape,
+        colors = colors,
+        contentPadding = textFieldPadding,
+        singleLine = true,
+    )
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun CityAddressField(
+    onValueChange: (String) -> Unit,
+    textStyle: TextStyle,
+    shape: Shape,
+    colors: TextFieldColors,
+    textFieldPadding: PaddingValues,
+    modifier: Modifier = Modifier,
+    initialValue: String = "",
+    label: @Composable () -> Unit,
+) {
+    var value by rememberSaveable { mutableStateOf(initialValue) }
+
+    PaddableOutlinedTextField(
+        modifier = modifier.autofill(
+            autofillTypes = listOf(AutofillType.AddressLocality),
+            onFill = {
+                value = it
+                onValueChange(it)
+            },
+        ),
+        value = value,
+        onValueChange = {
+            value = it
+            onValueChange(it)
+        },
+        label = label,
+        textStyle = textStyle,
+        shape = shape,
+        colors = colors,
+        contentPadding = textFieldPadding,
+        singleLine = true,
+    )
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun StateAddressField(
+    onValueChange: (String) -> Unit,
+    textStyle: TextStyle,
+    shape: Shape,
+    colors: TextFieldColors,
+    textFieldPadding: PaddingValues,
+    modifier: Modifier = Modifier,
+    initialValue: String = "",
+    label: @Composable () -> Unit,
+) {
+    var value by rememberSaveable { mutableStateOf(initialValue) }
+
+    PaddableOutlinedTextField(
+        modifier = modifier.autofill(
+            autofillTypes = listOf(AutofillType.AddressRegion),
+            onFill = {
+                value = it
+                onValueChange(it)
+            },
+        ),
+        value = value,
+        onValueChange = {
+            value = it
+            onValueChange(it)
+        },
+        label = label,
+        textStyle = textStyle,
+        shape = shape,
+        colors = colors,
+        contentPadding = textFieldPadding,
+        singleLine = true,
+    )
+}
